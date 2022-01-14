8607f50a95e750355a9653529696c6318d68f9cc
fherrerao <fherrera0206@gmail.com>
2022-01-14 16:13:26 -0500

    Fix linters errors

--- src/functions.js
+++ src/functions.js
@@ -2,53 +2,52 @@ const listContainer = document.querySelector('.list-container');
 const itemsContainer = document.querySelector('.list-container');
 
 export default class Collection {
-    static tasks = [];
-    static createTask = (task) => {    
-      let objTasks = {
-        description: task,
-        completed: false,
-        index: Collection.tasks.length+1
-      }
-      Collection.tasks.push(objTasks);  
-      return objTasks;
-    }
+  static tasks = [];
 
-    static showTasks = () => {
-        listContainer.innerHTML = '';  
-        Collection.tasks = JSON.parse(localStorage.getItem('tasks'));
-        if(!Collection.tasks){
-          Collection.tasks = [];    
-        }
-        else {    
-          Collection.tasks.forEach(element => {
-            const items = document.createElement('li');
-            items.classList.add('item-list');
-            items.id = element.index-1;
-            items.innerHTML = `
+  static createTask = (task) => {
+    const objTasks = {
+      description: task,
+      completed: false,
+      index: Collection.tasks.length + 1,
+    };
+    Collection.tasks.push(objTasks);
+    return objTasks;
+  };
+
+  static showTasks = () => {
+    listContainer.innerHTML = '';
+    Collection.tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (!Collection.tasks) {
+      Collection.tasks = [];
+    } else {
+      Collection.tasks.forEach((element) => {
+        const items = document.createElement('li');
+        items.classList.add('item-list');
+        items.id = element.index - 1;
+        items.innerHTML = `
               <input type="checkbox" id="id-${element.index}">        
               <p class="label" contenteditable="true">${element.description}</p>
               <box-icon class="icon" name='dots-vertical-rounded'></box-icon>
               <box-icon class="trash-icon d-none" name='trash'></box-icon>`;
-              
-            itemsContainer.appendChild(items);
-            
-          });
-        }
-    };
 
-    static deleteTask = (del) => {  
-        let indexArray;
-        Collection.tasks.forEach((element, index) => {    
-          if(element.description === del){
-            indexArray = index;
-          }
-        })
-        Collection.tasks.splice(indexArray,1)
-        Collection.setLocalStorage();
-        location.reload();
-      }
-
-    static setLocalStorage = () => {
-        localStorage.setItem('tasks', JSON.stringify(Collection.tasks));  
+        itemsContainer.appendChild(items);
+      });
     }
-  }
+  };
+
+  static deleteTask = (del) => {
+    let indexArray;
+    Collection.tasks.forEach((element, index) => {
+      if (element.description === del) {
+        indexArray = index;
+      }
+    });
+    Collection.tasks.splice(indexArray, 1);
+    Collection.setLocalStorage();
+    window.location.reload();
+  };
+
+  static setLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(Collection.tasks));
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,55 +1,53 @@
 import './style.css';
-import Collection from './functions'
+import Collection from './functions.js';
 
 document.addEventListener('DOMContentLoaded', Collection.showTasks());
 
 const trashIcon = document.querySelectorAll('.trash-icon');
-const iconDots = document.querySelectorAll('.icon') 
-for (let i=0; i<Collection.tasks.length; i += 1){
-  iconDots[i].setAttribute('id',i);  
+const iconDots = document.querySelectorAll('.icon');
+for (let i = 0; i < Collection.tasks.length; i += 1) {
+  iconDots[i].setAttribute('id', i);
 }
 
 const label = document.querySelectorAll('.label');
-label.forEach(item => {
-  item.addEventListener('click', (e) => {  
+label.forEach((item) => {
+  item.addEventListener('click', (e) => {
     e.preventDefault();
-    let task = e.currentTarget.innerHTML;    
-    let index = e.currentTarget.nextSibling.nextSibling.id;    
-    trashIcon[index].classList.remove('d-none')
+    const task = e.currentTarget.innerHTML;
+    const index = e.currentTarget.nextSibling.nextSibling.id;
+    trashIcon[index].classList.remove('d-none');
     trashIcon[index].addEventListener('click', () => {
       Collection.deleteTask(task);
-      location.reload();      
-    })
-    iconDots[index].classList.add('d-none')            
+      window.location.reload();
+    });
+    iconDots[index].classList.add('d-none');
   });
-})
+});
 
-label.forEach(item => {
-  item.addEventListener('blur', (e) => {  
-    e.preventDefault();    
-    let index = e.currentTarget.nextSibling.nextSibling.id;    
+label.forEach((item) => {
+  item.addEventListener('blur', (e) => {
+    e.preventDefault();
+    const index = e.currentTarget.nextSibling.nextSibling.id;
     setTimeout(() => {
-      trashIcon[index].classList.add('d-none')    
-      iconDots[index].classList.remove('d-none') 
-    }, 90);                 
+      trashIcon[index].classList.add('d-none');
+      iconDots[index].classList.remove('d-none');
+    }, 90);
   });
-})
+});
 
-label.forEach(item => {
-  item.addEventListener('input', (e) => {    
-    let index = e.currentTarget.nextSibling.nextSibling.id;
-    Collection.tasks[index].description = item.innerHTML
-    Collection.setLocalStorage();    
-  })  
-})
-
+label.forEach((item) => {
+  item.addEventListener('input', (e) => {
+    const index = e.currentTarget.nextSibling.nextSibling.id;
+    Collection.tasks[index].description = item.innerHTML;
+    Collection.setLocalStorage();
+  });
+});
 
 const inputDescription = document.querySelector('.add');
 window.addEventListener('keydown', (e) => {
-  if (e.keyCode === 13 && inputDescription.value !== "") {    
+  if (e.keyCode === 13 && inputDescription.value !== '') {
     Collection.createTask(inputDescription.value);
-    Collection.setLocalStorage(); 
-    location.reload();
-    input.value = '';
+    Collection.setLocalStorage();
+    window.location.reload();
   }
 });
