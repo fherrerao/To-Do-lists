69d2c155860da77774411227fac4cd30d52740c7
fherrerao <fherrera0206@gmail.com>
2022-01-14 23:19:21 -0500

    Add clear function

---
+++ src/status.js
@@ -0,0 +1,10 @@
+import Collection from "./functions.js";
+
+const clear = () => {
+  const result = Collection.tasks.filter((e) => e.completed === false);
+  Collection.tasks = result;
+  Collection.setLocalStorage();
+  window.location.reload();
+};
+
+export { clear as default };
